7c1236641fb5af4e6a7955af2b751a8beeb179e8
DustinSchouten <dustinschouten2@gmail.com>
2022-06-01 09:32:55 +0200

    test

--- javascript.js
+++ javascript.js
@@ -1,46 +1,39 @@
 const aantal_vragen = vragenlijst['questions'].length;
-let vraag_nummer = 1;
-
-function renderVorigeVraag() {
-    if (vraag_nummer > 1) {
-        vraag_nummer -= 1;
-        renderVraag(vraag_nummer);
-    }
-}
-
-function renderVolgendeVraag() {
-    if (vraag_nummer < aantal_vragen) {
-        vraag_nummer += 1;
-        renderVraag(vraag_nummer);
-    }
-}
+const container_element = document.querySelector('#container'); // Alle variabelenamen met '_element' staan voor opgehaalde DOM elementen.
 
 function renderVraag(vraag_nummer) {
-    document.querySelector('#button_vorige_vraag').classList.remove('invisible');
-    document.querySelector('#button_volgende_vraag').classList.remove('invisible');
-    if (vraag_nummer == 1) {
-        document.querySelector('#button_vorige_vraag').classList.add('invisible');
-    }
-    if (vraag_nummer == aantal_vragen) {
-        document.querySelector('#button_volgende_vraag').classList.add('invisible');
-    }
-
-    const vraag_object = vragenlijst['questions'][vraag_nummer-1];
+    const vraag_object = vragenlijst['questions'][vraag_nummer];
     const vraag_type = vraag_object['type']; // Er zijn verschillende soorten vragen, zoals open vragen (type=string) en multiple-choice vragen (type=choice).
-    const vraag_stelling = vraag_object['text'];
     const vraag_id = vraag_object['id'];
 
-    let vraag_stelling_element = document.querySelector('#vraag_stelling'); // Alle variabelenamen met '_element' staan voor opgehaalde DOM elementen.
+    // Voor iedere vraag een vraagcontainer genereren en renderen
+    let vraag_container_html_string = '<section class="vraag_container" data-id="vraag_'+(vraag_nummer+1).toString()+'">';
+    container_element.insertAdjacentHTML('afterbegin',vraag_container_html_string); // Alle variabelenamen met _html_string staan voor HTML dat gegenereerd wordt en getoond wordt in de app
+    let created_vraag_container_element = document.querySelector('[data-id="vraag_'+(vraag_nummer+1).toString()+'"]');
+
+    // Het vraagnummer genereren en renderen
+    let vraag_nummer_element = document.createElement('p');
+    vraag_nummer_element.classList.add('vraag_nummer');
+    vraag_nummer_element.textContent = 'Vraag ' + (vraag_nummer+1).toString() + ' / ' + aantal_vragen.toString();
+    created_vraag_container_element.appendChild(vraag_nummer_element);
+
+    // De vraagstelling genereren en renderen
+    const vraag_stelling = vraag_object['text'];
+    let vraag_stelling_element = document.createElement('p');
+    vraag_stelling_element.classList.add('vraag_stelling');
     vraag_stelling_element.textContent = vraag_stelling;
-    let vraag_nummer_element = document.querySelector('#vraag_nummer');
-    vraag_nummer_element.textContent = 'Vraag ' + vraag_nummer.toString() + ' / ' + aantal_vragen.toString();
+    created_vraag_container_element.appendChild(vraag_stelling_element);
 
-    let antwoord_invoer_section_element = document.querySelector('#antwoord_invoer');
-    antwoord_invoer_section_element.replaceChildren() // Verwijder alles uit de section.
+    // Een section genaamd antwoord_invoer genereren en renderen. In deze section worden de invoervelden en multiple-choice opties geplaatst.
+    let antwoord_invoer_html_string = '<section class="antwoord_invoer_container"></section>';
+    created_vraag_container_element.insertAdjacentHTML('beforeend',antwoord_invoer_html_string);
+    let created_antwoord_invoer_container_element = document.querySelector('.antwoord_invoer_container');
 
+
+    // De antwoord
     if (vraag_type == 'string') {
-        const antwoord_input_html_string = '<input type="text" id="antwoord_input"></input>'; // Alle variabelenamen met _html_string staan voor HTML dat gegenereerd wordt en getoond wordt in de app
-        antwoord_invoer_section_element.insertAdjacentHTML('afterbegin', antwoord_input_html_string);
+        const antwoord_input_html_string = '<input type="text" class="antwoord_input"></input>';
+        created_antwoord_invoer_container_element.insertAdjacentHTML('beforeend', antwoord_input_html_string);
     }
     if (vraag_type == 'choice' || vraag_type == 'open-choice') {
         const antwoord_opties_lijst = vraag_object['options'];
@@ -48,19 +41,20 @@ function renderVraag(vraag_nummer) {
             const antwoord_optie = antwoord_optie_object['text'];
             const antwoord_optie_id = antwoord_optie_object['id'];
             const antwoord_optie_html_string = '<section class="antwoord_optie"><input type="radio" name="'+vraag_id+'" id="'+antwoord_optie_id+'"><label for="'+antwoord_optie_id+'">'+antwoord_optie+'</label></section>';
-            antwoord_invoer_section_element.insertAdjacentHTML('beforeend', antwoord_optie_html_string);
+            created_antwoord_invoer_container_element.insertAdjacentHTML('beforeend', antwoord_optie_html_string);
         })
     }
     if (vraag_type == 'open-choice') {
-        const antwoord_input_html_string = '<input type="text" id="antwoord_input"</input>'; // Alle variabelenamen met _html_string staan voor HTML dat gegenereerd wordt en getoond wordt in de app
-        antwoord_invoer_section_element.insertAdjacentHTML('beforeend', antwoord_input_html_string);
+        const antwoord_input_html_string = '<input type="text" class="antwoord_input"</input>';
+        created_antwoord_invoer_container_element.insertAdjacentHTML('beforeend', antwoord_input_html_string);
     }
 }
 
 
 window.onload = () => {
-    const button_vorige_vraag = document.querySelector('#button_vorige_vraag').addEventListener('click',renderVorigeVraag);
-    const button_volgende_vraag = document.querySelector('#button_volgende_vraag').addEventListener('click',renderVolgendeVraag);
-    renderVraag(1);
-
+    // const button_vorige_vraag = document.querySelector('#button_vorige_vraag').addEventListener('click',renderVorigeVraag);
+    // const button_volgende_vraag = document.querySelector('#button_volgende_vraag').addEventListener('click',renderVolgendeVraag);
+    for (let vraag_idx=aantal_vragen-1; vraag_idx>-1; vraag_idx--) {
+        renderVraag(vraag_idx);
+    }
 }
